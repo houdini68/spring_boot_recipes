da7d9683d88678e9156ba36ae85754d5965368a4
m999rvi <Rudy.Vissers@m-team.be>
2022-04-10 07:36:10 +0200

    recipe on consuming a RESTful web service

---
+++ src/main/java/com/rudyvissers/learn/spf/consumingrest/Book.java
@@ -0,0 +1,63 @@
+package com.rudyvissers.learn.spf.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Book {
+    private List<String> authors = new ArrayList<>();
+    private String isbn;
+    private String titles;
+
+    public Book() {
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitles() {
+        return titles;
+    }
+
+    public void setTitles(String titles) {
+        this.titles = titles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return isbn.equals(book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "authors=" + authors +
+                ", isbn='" + isbn + '\'' +
+                ", titles='" + titles + '\'' +
+                '}';
+    }
+}
